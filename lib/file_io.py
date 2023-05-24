e8649198e473132452598990ff3b2302ba3b76a8
faith wangare <faithwangare750@gmail.com>
2023-05-24 13:37:57 +0300

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,18 +1,15 @@
 def write_file(file_name, file_content):
-    file_path = str(file_name) + '.txt' 
-    with open(file_path, mode ='w') as file:
-        file.write(file_content)
+    file_path= str(file_name) + ".txt"
+    with open(file_path, 'w') as f:
+        f.write(file_content)
     
 
 def append_file(file_name, append_content):
-    file_path = str(file_name) + '.txt' 
-    with open(file_path, mode ='a') as file:
-        file.write(append_content)
-    
+    file_path= str(file_name) + ".txt"
+    with open(file_path, 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    file_path = str(file_name) + '.txt' 
-    with open(file_path, mode = "r") as file:
-        file_content = file.read()
-        return file_content
-    
+    file_path= str(file_name) + ".txt"
+    with open(file_path, 'r') as f:
+        return f.read()
